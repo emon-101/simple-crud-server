56351c4e6f4f34f1608a530eb88ea000cfbe9764
emon-101 <emon26.ugv@gmail.com>
2026-05-07 12:45:25 +0600

    get dynamic user data from the server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -32,6 +32,15 @@ const run = async() => {
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        app.get('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const user = await userCollections.findOne(query);
+            res.send(user);
+        })
 
         await client.db("admin").command({ping: 1});
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
