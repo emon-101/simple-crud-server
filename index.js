ff98dada9b04bf5a4e74b40a41b7f02a15ee7b21
emon-101 <emon26.ugv@gmail.com>
2026-05-08 18:47:58 +0600

    add update user functionality to the API

--- index.js
+++ index.js
@@ -48,6 +48,24 @@ const run = async() => {
             res.send(result);
         })
 
+        app.patch('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const modifiedUser = req.body;
+            const updatedDocument = {
+                $set: {
+                    name: modifiedUser.name,
+                    email: modifiedUser.email,
+                    role: modifiedUser.role
+                }
+            }
+
+            const result = await userCollections.updateOne(filter, updatedDocument);
+            res.send(result);
+        })
+
         app.delete('/users/:id', async(req, res) => {
             const id = req.params.id;
             const query = {
